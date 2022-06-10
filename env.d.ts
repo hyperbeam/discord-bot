8ab5957ff9c711450d98d15c95aca26854648622
rjt <rjt.rockx@gmail.com>
2022-06-11 04:15:21 +0530

    Switch to slash-create

--- env.d.ts
+++ env.d.ts
@@ -1,9 +1,10 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      DISCORD_APP_ID: string;
+      DISCORD_BOT_TOKEN: string;
+      DISCORD_DEVELOPMENT_GUILD_ID: string;
       HYPERBEAM_API_KEY: string;
-      BOT_TOKEN: string;
-      BOT_CLIENT_ID: string;
     }
   }
 }
---
+++ src/commands/ping.ts
@@ -0,0 +1,15 @@
+import { SlashCommand, SlashCreator, CommandContext } from 'slash-create';
+
+export default class Ping extends SlashCommand {
+	constructor(creator: SlashCreator) {
+		super(creator, {
+			name: 'ping',
+			description: 'Replies with pong!'
+		});
+		this.filePath = __filename;
+	}
+
+	async run(ctx: CommandContext) {
+		return ctx.send('Pong from slash-create!');
+	}
+}
