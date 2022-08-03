9037ae8bd0f9c20b13040de8e275161ca4c0b2c4
rjt <rjt.rockx@gmail.com>
2022-08-03 07:43:05 +0530

    Bugfix

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vite";
 import dotenv from "dotenv";
-dotenv.config({ path: "./.env" });
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 import sveltePreprocess from "svelte-preprocess";
 
+dotenv.config({ path: "./.env" });
 const port = parseInt(process.env.VITE_CLIENT_PORT || "4000", 10);
 
 // https://vitejs.dev/config/
