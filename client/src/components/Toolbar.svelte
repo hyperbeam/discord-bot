fe85525671e5e0d4a35802e6c1b20025c732416c
Porasjeet Singh <porasjeet@gmail.com>
2022-09-07 05:16:26 +0530

    Add signin signout icon buttons

--- client/src/components/Toolbar.svelte
+++ client/src/components/Toolbar.svelte
@@ -1,5 +1,9 @@
 <script>
+  import { navigate } from "svelte-navigator";
+  import { logoutUser } from "../scripts/api";
+  import { currentUser } from "../scripts/state";
   import Members from "./Members.svelte";
+  import Tooltip from "./Tooltip.svelte";
   import Volume from "./Volume.svelte";
 
   $: isFullscreen = false;
@@ -37,6 +41,34 @@
           />
         </svg>
       </div>
+      {#if $currentUser}
+        <Tooltip text="Sign out">
+          <div class="icon" on:click={logoutUser}>
+            <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M10,17V14H3V10H10V7L15,12L10,17M10,2H19A2,2 0 0,1 21,4V20A2,2 0 0,1 19,22H10A2,2 0 0,1 8,20V18H10V20H19V4H10V6H8V4A2,2 0 0,1 10,2Z"
+              />
+            </svg>
+          </div>
+        </Tooltip>
+      {:else}
+        <Tooltip text="Sign in">
+          <div
+            class="icon"
+            on:click={() => {
+              navigate("/authorize");
+            }}
+          >
+            <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+              <path
+                fill="currentColor"
+                d="M16,17V14H9V10H16V7L21,12L16,17M14,2A2,2 0 0,1 16,4V6H14V4H5V20H14V18H16V20A2,2 0 0,1 14,22H5A2,2 0 0,1 3,20V4A2,2 0 0,1 5,2H14Z"
+              />
+            </svg>
+          </div>
+        </Tooltip>
+      {/if}
     </div>
   </div>
 {/if}
