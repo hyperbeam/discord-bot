0bbb7ca9481acbb1c8baaf03e40e08b20541ce34
rjt <rjt.rockx@gmail.com>
2022-09-18 00:03:49 +0530

    Fix types

--- client/src/scripts/state.ts
+++ client/src/scripts/state.ts
@@ -1,10 +1,10 @@
-import { HyperbeamIFrame } from "@hyperbeam/web";
+import { HyperbeamEmbed } from "@hyperbeam/web";
 import { writable } from "svelte/store";
 import { Room, User } from "./types";
 
 export const currentUser = writable<User | null>(null);
 export const currentRoom = writable<Room | null>(null);
-export const hb = writable<HyperbeamIFrame | null>(null);
+export const hb = writable<HyperbeamEmbed | null>(null);
 
 export const rooms = writable<Room[]>();
-export const hbSession = writable<HyperbeamIFrame>();
+export const hbSession = writable<HyperbeamEmbed>();
