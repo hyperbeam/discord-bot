e7de9cc3ce17cff3c220dd2dbe803d8ba15fd0e5
rjt <rjt.rockx@gmail.com>
2022-10-07 21:22:29 +0530

    Cleanup unused dependencies

--- bot/types.d.ts
+++ bot/types.d.ts
@@ -1,21 +1,7 @@
 import { Client } from "discord.js";
 import Database from "./classes/database";
-import { Session, SessionData } from "express-session";
 
 // slash-create
 export interface BotClient extends Client {
 	db: typeof Database;
-}
-
-// express-session
-declare module "http" {
-	interface IncomingMessage {
-		session: Session & Partial<SessionData>;
-	}
-}
-
-declare module "express-session" {
-	export interface SessionData {
-		authenticated?: boolean;
-	}
 }
