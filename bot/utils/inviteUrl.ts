8cc123982268ab518a42187f666a046fc48b0676
rjt <rjt.rockx@gmail.com>
2022-10-14 04:13:13 +0530

    Fix invite url

--- bot/utils/inviteUrl.ts
+++ bot/utils/inviteUrl.ts
@@ -1,9 +1,9 @@
 const inviteUrl = new URL("https://discord.com/api/oauth2/authorize");
 inviteUrl.search = new URLSearchParams({
 	client_id: process.env.VITE_CLIENT_ID,
-	redirect_uri: encodeURIComponent(process.env.VITE_CLIENT_BASE_URL + "/authorize"),
+	redirect_uri: process.env.VITE_CLIENT_BASE_URL + "/authorize",
 	response_type: "code",
-	scope: encodeURIComponent("identify email bot applications.commands"),
+	scope: "identify email bot applications.commands",
 	permissions: "277062470720",
 }).toString();
 
