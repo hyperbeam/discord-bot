d65666bb80f01b5d18364502305d6082f2e44ffc
rjt <rjt.rockx@gmail.com>
2022-10-23 17:29:49 +0530

    Make rooms private

--- bot/classes/room.ts
+++ bot/classes/room.ts
@@ -39,6 +39,7 @@ export class BotRoom extends Room<RoomState> {
 		this.roomId = options.url || nanoid();
 		this.setState(new RoomState());
 		this.setPatchRate(40);
+		this.setPrivate(true);
 		this.state.ownerId = options.ownerId;
 		await this.registerMessageHandlers();
 		await startSession({ room: this, options });
